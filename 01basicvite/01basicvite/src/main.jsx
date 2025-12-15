710369d831871c8d9287eeea0389030ade185853
Aditya Bhosale <adityabhosale4388@gmail.com>
2025-12-15 17:34:13 +0530

    add custom react and changes in vite

--- 01basicvite/01basicvite/src/main.jsx
+++ 01basicvite/01basicvite/src/main.jsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Youtube from './Youtube.jsx';
 
+function Myapp(){
+  return (
+    <div>
+      <h1>Hello from my react app</h1>
+    </div>
+  )
+}
+
+const areactElement = React.createElement(
+  'a',
+  {href: "https://www.youtube.com" , target: "_blank"},
+  'Go to Youtube'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode> 
-    <App />
-    <Youtube />
-  </React.StrictMode>
+ areactElement
 );
